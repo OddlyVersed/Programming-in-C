20963b3984cf2b275925f3807a3aff5b37e8587c
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-08-31 18:28:19 +0530

    Update Experiment 1.2.c

--- Experiment 1/Experiment 1.2.c
+++ Experiment 1/Experiment 1.2.c
@@ -1,1 +1,18 @@
+/*Experiment 1: installation, Environment setup and starting with C language
+2.Write a C Program to print the address in multiple lines (new line).
+*/
+#include <stdio.h>
+ int main() 
+{
+   printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+
+    printf("*\n");
+    printf("***\n");
+    printf("*****\n");
+    printf("*******");
+
+    printf("\n--------------------------------\n");
+    return '0';
+}
 
