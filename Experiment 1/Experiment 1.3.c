ef06427d98d7bef553acc4ce2f9a3ce7648bf2a7
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-08-31 19:13:44 +0530

    Update Experiment 1.3.c

--- Experiment 1/Experiment 1.3.c
+++ Experiment 1/Experiment 1.3.c
@@ -1,1 +1,15 @@
-
+/*Experiment 1: installation, Environment setup and starting with C language
+3. ⁠Write a C program to add two numbers and take a number from the user.
+*/
+#include <stdio.h>
+int main()
+{
+     printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    int a,b;
+    printf("enter the value a = "); 
+    scanf("%d",&a);
+    printf("enter the value b = "); 
+    scanf("%d",&b);
+    int sum=0;
+     sum = a + b;
