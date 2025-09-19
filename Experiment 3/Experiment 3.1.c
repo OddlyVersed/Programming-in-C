18e7d16a0c97c7784db1ecfd761fd4a5f91b95fe
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-09-19 15:07:17 +0530

    Update Experiment 3.1.c

--- Experiment 3/Experiment 3.1.c
+++ Experiment 3/Experiment 3.1.c
@@ -1,1 +1,22 @@
+/*Experiment 3.1: Conditional Statements
+1.⁠ ⁠Write a C program to check whether a number is Even or ODD*/
+#include <stdio.h>
+int main()
+{
+int a;
+printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    printf("Enter the number = ");
+	scanf("%d",&a);
+	
+	{
+		if (a%2==0)
+		printf("the number a is even");
+		
+		else 
+		printf("the number a is odd");
+	}
+	printf("\n--------------------------------\n");
+	return 0;
+}
 
