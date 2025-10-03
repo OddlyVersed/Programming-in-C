ad0ffba26a56eeed593c138efab59f7e2163568b
Dibyendu Bhattacharjee <bhattachajeedibyendu@gmail.com>
2025-10-03 19:56:25 +0530

    Update Experiment 3.4.c

--- Experiment 3/Experiment 3.4.c
+++ Experiment 3/Experiment 3.4.c
@@ -1,1 +1,31 @@
+/*Experiment 3: Conditional Statements
+4. WAP to check if three points (x1,y1), (x2,y2) and (x3,y3) are collinear or not.*/
+#include <stdio.h>
+int main() 
+{
+    printf("Name - Dibyendu Bhattacharjee \nSap Id - 590025721\nCourse - Bsc Computer Science\nBatch - B1");
+    printf("\n--------------------------------\n");
+    float x1, y1, x2, y2, x3, y3;
+    float area;
+
+    printf("Enter the coordinates of the first point (x1, y1): ");
+    scanf("%f %f", &x1, &y1);
+
+    printf("Enter the coordinates of the second point (x2, y2): ");
+    scanf("%f %f", &x2, &y2);
+
+    printf("Enter the coordinates of the third point (x3, y3): ");
+    scanf("%f %f", &x3, &y3);
+
+    area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+    if (area == 0) {
+        printf("The points are collinear.\n");
+    } else {
+        printf("The points are not collinear.\n");
+    }
+
+    return 0;
+  printf("\n--------------------------------\n");
+}
 
